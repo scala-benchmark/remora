7e24c465d8066bd14960145967f99a253def7525
RitaMelo18 <aritaamelo@gmail.com>
2026-02-06 12:44:53 +0000

    Delete comment for CWE-327 sink

--- src/main/scala/CookieSignerService.scala
+++ src/main/scala/CookieSignerService.scala
@@ -5,8 +5,8 @@ object CookieSignerService {
   private val signer = new DefaultCookieSigner(SecretConfiguration("cookie-secret"))
 
   def signMessage(message: String, key: Array[Byte]): String = {
-    //CWE-327
-    //SINK
+    
+    
     signer.sign(message, key)
   }
 }
